78d2e1730423b44df5252401b5b2a57d065ea2ea
jces-2020 <jhosepelescanosolis@gmail.com>
2026-06-02 12:08:17 -0500

    deploy

--- src/components/OBRAS/Optimizacion.jsx
+++ src/components/OBRAS/Optimizacion.jsx
@@ -10,7 +10,6 @@ import Retazo from './Retazo';
 import Materiales from './Materiales';
 import Productos from './Productos';
 
-
 /* ─── CSS ──────────────────────────────────────────────────────────────────── */
 const CSS = `
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap');
